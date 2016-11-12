db465c17986a11d8a05acd1d1d035816fbccff5b
grammophone <emmanuel.liossis@gmail.com>
2016-11-12 15:05:01 +0200

    Add IStorageProvider-IStorageClient contract.

--- AzureStorageProvider.cs
+++ AzureStorageProvider.cs
@@ -3,50 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace Grammophone.Storage.Azure
 {
 	/// <summary>
-	/// Azure implementation of a <see cref="IStorageProvider"/>.
+	/// <see cref="IStorageProvider"/> implementation for Azure.
+	/// It is thread-safe as required by the contract.
 	/// </summary>
 	public class AzureStorageProvider : IStorageProvider
 	{
-		#region Private fields
-
-		private CloudBlobClient cloudBlobClient;
-
-		#endregion
-
 		#region Construction
 
 		/// <summary>
 		/// Create.
 		/// </summary>
-		/// <param name="connectionString">The connection string to an Azure storage account.</param>
-		public AzureStorageProvider(string connectionString)
+		/// <param name="connectionString">The Azure connection string.</param>
+		/// <param name="urlBase">The URL base of the files provided by the azure account.</param>
+		public AzureStorageProvider(string connectionString, string urlBase)
 		{
 			if (connectionString == null) throw new ArgumentNullException(nameof(connectionString));
+			if (urlBase == null) throw new ArgumentNullException(nameof(urlBase));
 
-			var account = CloudStorageAccount.Parse(connectionString);
-
-			cloudBlobClient = account.CreateCloudBlobClient();
+			this.URLBase = urlBase;
 		}
+
+		#endregion
+
+		#region Public properties
+
+		/// <summary>
+		/// The URL base of the files provided by the azure account.
+		/// </summary>
+		public string URLBase { get; private set; }
+
+		/// <summary>
+		/// The Azure connection string.
+		/// </summary>
+		public string ConnectionString { get; private set; }
 
 		#endregion
 
 		#region Public methods
 
-		public async Task<IStorageContainer> GetContainerAsync(string containerName)
+		/// <summary>
+		/// Get a client for file operations.
+		/// </summary>
+		public IStorageClient GetClient()
 		{
-			if (containerName == null) throw new ArgumentNullException(nameof(containerName));
-
-			var cloudBlobContainer = cloudBlobClient.GetContainerReference(containerName);
-
-			if (!(await cloudBlobContainer.ExistsAsync())) return null;
-
-			return new AzureStorageContainer(cloudBlobContainer);
+			return new AzureStorageClient(this.ConnectionString);
 		}
 
 		#endregion
